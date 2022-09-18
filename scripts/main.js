a4a4a3b2c662f0a3e10cd17c8d325545d5a9d8f5
cprincec <chukwuprincec@gmail.com>
2022-09-18 22:22:47 +0100

    portfolio

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+const ol = document.querySelector("#list");
+const links = [
+    {
+        label: "Week 1 notes",
+        url: "week1/index.html",
+        description: "Local Storage and Master Mobile UX"
+    }
+];
+
+links.forEach(link => {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    let div = document.createElement("div");
+    a.setAttribute("href", link.url);
+    a.innerHTML = link.label;
+    div.innerHTML = link.description;
+
+    div.setAttribute("class", "description");
+
+    li.append(a);
+    li.append(div);
+    ol.append(li);
+});
+
+
+
+
